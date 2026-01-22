b90dab1873d7d9485182de7ee6f59103a1798d40
Agillis24 <166214892+Agillis24@users.noreply.github.com>
2026-01-22 13:03:31 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@
   import path from 'path';
 
   export default defineConfig({
-    plugins: [react()],
+  base: '/senior_taxi/',  // <-- přidej tento řádek
+  plugins: [react()],,
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
